8d4535765532b97c70bddbc5e9ac72e6c36ce3be
Himanshu Bobade <himanshubobade96@gmail.com>
2023-07-09 23:45:39 +0530

    basic hello world display on server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.json({ hello :"world" });
+});
+
+app.listen(5000, () => {
+    console.log(`listening n port 5000`);
+});
