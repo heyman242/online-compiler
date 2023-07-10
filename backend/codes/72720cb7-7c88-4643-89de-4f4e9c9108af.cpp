1eb7c640958c8fa22ec3f8c87c55e4f5631ea538
Himanshu Bobade <himanshubobade96@gmail.com>
2023-07-10 18:36:52 +0530

    function to create a new cpp file in directory

---
+++ backend/codes/72720cb7-7c88-4643-89de-4f4e9c9108af.cpp
@@ -0,0 +1,6 @@
+#include<studio.h>
+#include<iostream>
+int main() {
+printf("hello world \n")
+return 0;
+}
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const { generateFile } = require('./generateFile')
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
 app.get('/', (req, res) => {
     return res.json({ hello :"world" });
+});
+
+app.post('/run', async (req,res) => {
+    const {language = "cpp" , code } = req.body;
+    if(code === undefined){
+        return res.status(400).json({success : false, error :"empty code body "})
+    }
+    // need to generate a c++ file with content from the request
+    const filepath = await generateFile(language, code);
+    return res.status(200).json({filepath});
 });
 
 app.listen(5000, () => {
